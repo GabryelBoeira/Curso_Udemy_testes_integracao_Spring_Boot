2a142b788d1d2ccf6e67eaafaeee8186d2d8c762
Gabryel Boeira <gabryel392@gmail.com>
2021-04-02 18:02:01 -0300

    Novo Projeto para a utilização do MokiMvc

---
+++ ContatosMockMvc/src/main/java/com/br/gabryel/ContatosMockMvc/ContatosMockMvcApplication.java
@@ -0,0 +1,13 @@
+package com.br.gabryel.ContatosMockMvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatosMockMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatosMockMvcApplication.class, args);
+	}
+
+}
