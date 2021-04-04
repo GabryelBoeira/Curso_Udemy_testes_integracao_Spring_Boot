5e7721381579e189ca46f0b309e65be50a52bbe5
Gabryel Boeira <gabryel392@gmail.com>
2021-04-04 17:47:08 -0300

    Trabalhando com os diferentes tipos MockMvcResultMatchers para realização de validações

--- ContatosMockMvc/src/test/java/com/br/gabryel/ContatosMockMvc/controller/AgendaControllerIntegrationTest.java
+++ ContatosMockMvc/src/test/java/com/br/gabryel/ContatosMockMvc/controller/AgendaControllerIntegrationTest.java
@@ -2,6 +2,8 @@ package com.br.gabryel.ContatosMockMvc.controller;
 
 import javax.transaction.Transactional;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,6 +17,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.ModelResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
+import org.springframework.test.web.servlet.result.ViewResultMatchers;
 
 import com.br.gabryel.ContatosMockMvc.model.Contato;
 
@@ -33,7 +38,7 @@ public class AgendaControllerIntegrationTest {
 	private Contato contato;
 
 	@BeforeEach
-	public void start() {
+	public void start() throws Exception {
 
 		contato = new Contato("41", "123451234", "Chefe");
 		testEntityManager.persist(contato);
@@ -50,6 +55,48 @@ public class AgendaControllerIntegrationTest {
 
 		ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/agenda/"));
 		resultActions.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	@Test
+	public void verificarStatusRequsicao() throws Exception {
+
+		ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/agenda/"));
+		StatusResultMatchers statusResult = MockMvcResultMatchers.status();
+
+		resultActions.andExpect(statusResult.isOk());
+
+		resultActions.andExpect(statusResult.is(200));
+
+		resultActions.andExpect(statusResult.is(Matchers.is(200)));
+	}
+
+	@Test
+	public void verificarViewRequisicao() throws Exception {
+
+		ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/agenda/"));
+		ViewResultMatchers viewResult = MockMvcResultMatchers.view();
+
+		resultActions.andExpect(viewResult.name("agenda"));
+
+		resultActions.andExpect(viewResult.name(Matchers.is("agenda")));
+	}
+
+	@Test
+	public void verificarResultadoRequisicao() throws Exception {
+
+		ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/agenda/"));
+		ModelResultMatchers modelResult = MockMvcResultMatchers.model();
+
+		resultActions.andExpect(modelResult.attributeExists("contatos"));
+
+		resultActions.andExpect(modelResult.attribute("contatos", Matchers.hasSize(1)));
+
+		resultActions.andExpect(modelResult.attribute("contatos",
+				Matchers.hasItem(Matchers.allOf(Matchers.hasProperty("id", Matchers.is(contato.getId())),
+						Matchers.hasProperty("nome", Matchers.is(contato.getNome())),
+						Matchers.hasProperty("ddd", Matchers.is(contato.getDdd())),
+						Matchers.hasProperty("telefone", Matchers.is(contato.getTelefone()))))));
+
 	}
 
 }
