fc21b334814b5f2793097e27fd429400018e3d57
Gabryel Boeira <gabryel392@gmail.com>
2021-04-05 00:55:26 -0300

    Projeto base para teste com realização de auditorias

---
+++ envers/src/main/java/com/br/gabryel/envers/EnversApplication.java
@@ -0,0 +1,13 @@
+package com.br.gabryel.envers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnversApplication.class, args);
+	}
+
+}
---
+++ envers/src/main/java/com/br/gabryel/envers/repository/ContatoRepository.java
@@ -0,0 +1,9 @@
+package com.br.gabryel.envers.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.gabryel.envers.model.Contato;
+
+public interface ContatoRepository extends JpaRepository<Contato, Long>{
+
+}
