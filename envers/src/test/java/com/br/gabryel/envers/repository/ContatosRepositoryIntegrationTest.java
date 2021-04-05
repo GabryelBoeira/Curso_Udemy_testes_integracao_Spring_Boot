440407466f18678d72f637af5de1504ff0058e25
Gabryel Boeira <gabryel392@gmail.com>
2021-04-05 00:58:44 -0300

    Utilizando AuditReader e AuditQuery

---
+++ envers/src/test/java/com/br/gabryel/envers/repository/ContatosRepositoryIntegrationTest.java
@@ -0,0 +1,143 @@
+package com.br.gabryel.envers.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.envers.AuditReader;
+import org.hibernate.envers.AuditReaderFactory;
+import org.hibernate.envers.RevisionType;
+import org.hibernate.envers.query.AuditEntity;
+import org.hibernate.envers.query.AuditQuery;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.br.gabryel.envers.model.Contato;
+
+@SpringBootTest
+public class ContatosRepositoryIntegrationTest {
+
+	@Autowired
+	private ContatoRepository contatoRepository;
+
+	@Autowired
+	private EntityManagerFactory entityManager;
+
+	private Contato contato;
+
+	@BeforeEach
+	public void start() {
+		contato = new Contato("Chefe", "0y", "9xxxxxxx9");
+	}
+
+	@Test
+	public void auditoriaAposSalvarRetornaObjetoCorreto() {
+
+		contatoRepository.save(contato);
+
+		AuditReader reader = AuditReaderFactory.get(entityManager.createEntityManager());
+
+		Contato contatoVindoDaAuditoria = reader.find(Contato.class, contato.getId(), new Date());
+
+		assertTrue("Chefe".equals(contatoVindoDaAuditoria.getNome()));
+		assertEquals(contato, contatoVindoDaAuditoria);
+
+	}
+
+	@Test
+	public void auditoriaAposSalvarRetornaObjetoCorretoComAuditQuery() {
+
+		contatoRepository.save(contato);
+
+		AuditReader reader = AuditReaderFactory.get(entityManager.createEntityManager());
+
+		AuditQuery query = reader.createQuery().forRevisionsOfEntity(Contato.class, true, true)
+				.add(AuditEntity.property("id").eq(contato.getId()))
+				.add(AuditEntity.revisionType().eq(RevisionType.ADD));
+
+		Contato contatoVindoDaAuditoria = (Contato) query.getSingleResult();
+
+		assertTrue("Chefe".equals(contatoVindoDaAuditoria.getNome()));
+		assertEquals(contato, contatoVindoDaAuditoria);
+
+	}
+
+	@Test
+	public void auditoriaAposEdicaoRetornaObjetoCorreto() {
+		
+		contatoRepository.save(contato);
+
+		contato.setNome("Novo Chefe");
+		contatoRepository.save(contato);
+
+		AuditReader reader = AuditReaderFactory.get(entityManager.createEntityManager());
+
+		Contato contatoVindoDaAuditoria = reader.find(Contato.class, contato.getId(), new Date());
+
+		assertTrue("Novo Chefe".equals(contatoVindoDaAuditoria.getNome()));
+
+	}
+
+	@Test
+	public void auditoriaAposEdicaoRetornaObjetoCorretoComAuditQuery() {
+		
+		contatoRepository.save(contato);
+
+		contato.setNome("Novo Chefe");
+		contatoRepository.save(contato);
+
+		AuditReader reader = AuditReaderFactory.get(entityManager.createEntityManager());
+
+		AuditQuery query = reader.createQuery().forRevisionsOfEntity(Contato.class, true, true)
+				.add(AuditEntity.property("id").eq(contato.getId()))
+				.add(AuditEntity.revisionType().eq(RevisionType.MOD));
+
+		Contato contatoVindoDaAuditoria = (Contato) query.getSingleResult();
+
+		assertTrue("Novo Chefe".equals(contatoVindoDaAuditoria.getNome()));
+	}
+
+	@Test
+	public void auditoriaAposExclusaoRetornaObjetoCorreto() {
+		
+		contatoRepository.save(contato);
+		contatoRepository.delete(contato);
+
+		AuditReader reader = AuditReaderFactory.get(entityManager.createEntityManager());
+
+		List<Number> numerosDasRevisoes = reader.getRevisions(Contato.class, contato.getId());
+		Number number = numerosDasRevisoes.get(numerosDasRevisoes.size() - 1);
+
+		Contato contatoVindoDaAuditoria = reader.find(Contato.class, Contato.class.getName(), contato.getId(), number,
+				true);
+
+		assertEquals(contato.getId(), contatoVindoDaAuditoria.getId());
+		assertNull(contatoVindoDaAuditoria.getNome());
+	}
+
+	@Test
+	public void auditoriaAposExclusaoRetornaObjetoCorretoComAuditQuery() {
+		
+		contatoRepository.save(contato);
+		contatoRepository.delete(contato);
+
+		AuditReader reader = AuditReaderFactory.get(entityManager.createEntityManager());
+
+		AuditQuery query = reader.createQuery().forRevisionsOfEntity(Contato.class, true, true)
+				.add(AuditEntity.property("id").eq(contato.getId()))
+				.add(AuditEntity.revisionType().eq(RevisionType.DEL));
+
+		Contato contatoVindoDaAuditoria = (Contato) query.getSingleResult();
+
+		assertEquals(contato.getId(), contatoVindoDaAuditoria.getId());
+		assertNull(contatoVindoDaAuditoria.getNome());
+	}
+
+}
